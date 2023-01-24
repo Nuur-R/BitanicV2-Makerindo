6382e075727d2cd6de723fdf0ffe1aed8b1c7cb8
Nuur-R <firdausnuurr@gmail.com>
2023-01-24 13:42:25 +0700

    Hampir mentok integrasi dengan deden

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,13 @@ String publisTopic = "bitanic";
 String subscribeTopic = "bitanic/" + id;
 
 // EEPROM Address
-int SetHariEEPROM = 190;
+int SundayEEPROM = 210;
+int MondayEEPROM = 220;
+int TuesdayEEPROM = 230;
+int WednesdayEEPROM = 240;
+int ThursdayEEPROM = 250;
+int FridayEEPROM = 260;
+int SaturdayEEPROM = 270;
 int JumlahMingguEEPROM = 200;
 int Minggu0EEPROM = 205;
 int SetOnTime1EEPROM = 100;
@@ -68,12 +74,10 @@ String motor2Status = "";
 float humidity = 0;
 float temperature = 0;
 float heatIndex = 0;
-String JadwalMinggu = "";
+String timeNow = "";
+String dateNow = "";
 String JadwalHari = "";
-int Hari[7];
-String JadwalWaktu = "";
-String JadwalWaktuMinute = 0;
-
+// int countEND = 0; // untuk menghitung jumlah END yang ada di EEPROM
 
 // LiquidCrystal_I2C Declaration
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
@@ -81,6 +85,13 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 // DHT Declaration
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
+
+int Detik, PortValue, countWifi, count2, State1, GlobalCount, h1, h2, LockTime, CountTime, CountMotor, TimeMotor, CountMinggu, TotalMinggu, OnDay, SaveOn, CountDayOn, countEND;
+float t1, t2;
+unsigned long GlobalClock;
+String DATA[10], INPUTDATA, TimeNOW, DateNOW;
+bool Lock1, Lock2, Lock3, Lock4, LockDate, MotorLock, LockMQTT;
+// String ssid, password;
 
 // RTC Declaration
 RTC_DS3231 rtc;
@@ -234,36 +245,92 @@ void callbackResponse(String topic, String payload){
         buzz(1500, 3);
       }
     }
-    else if (command == "SETHARI"){
-      EEPROM.writeString(SetHariEEPROM, payload);
+    else if (command == "SETHARI")
+    {
+      String TT;
+      if (getNumber(payload, 1) == "1")
+      {
+        EEPROM.writeString(SundayEEPROM, "Sunday");
+        TT = "1";
+      }
+      else
+      {
+        EEPROM.writeString(SundayEEPROM, "NONE");
+        TT = "0";
+      }
+      if (getNumber(payload, 2) == "1")
+      {
+        EEPROM.writeString(MondayEEPROM, "Monday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(MondayEEPROM, "NONE");
+        TT += "0";
+      }
+      if (getNumber(payload, 3) == "1")
+      {
+        EEPROM.writeString(TuesdayEEPROM, "Tuesday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(TuesdayEEPROM, "NONE");
+        TT += "0";
+      }
+      if (getNumber(payload, 4) == "1")
+      {
+        EEPROM.writeString(WednesdayEEPROM, "Wednesday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(WednesdayEEPROM, "NONE");
+        TT += "0";
+      }
+      if (getNumber(payload, 5) == "1")
+      {
+        EEPROM.writeString(ThursdayEEPROM, "Thursday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(ThursdayEEPROM, "NONE");
+        TT += "0";
+      }
+      if (getNumber(payload, 6) == "1")
+      {
+        EEPROM.writeString(FridayEEPROM, "Friday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(FridayEEPROM, "NONE");
+        TT += "0";
+      }
+      if (getNumber(payload, 7) == "1")
+      {
+        EEPROM.writeString(SaturdayEEPROM, "Saturday");
+        TT += "1";
+      }
+      else
+      {
+        EEPROM.writeString(SaturdayEEPROM, "NONE");
+        TT += "0";
+      }
+      EEPROM.writeString(300, TT);
       Serial.println("SETHARI OK");
       EEPROM.commit();
-      JadwalHari = EEPROM.readString(SetHariEEPROM);
-      // split string input dengan delimiter ","
-      String parts[8];
-      int i = 0;
-      int j = 0;
-      while (i < JadwalHari.length()) {
-        if (JadwalHari[i] == ',') {
-          j++;
-        } else {
-          parts[j] += JadwalHari[i];
-        }
-        i++;
-      }
-      // masukkan nilai dari bagian kedua hingga kedelapan ke dalam array Hari
-      for (i = 1; i <= 7; i++) {
-        Hari[i - 1] = parts[i].toInt();
-      }
       lcdPrint("SETHARI OK", "Commit");
       buzz(500, 3);
     }
     else if (command == "SETMINGGU")
     {
+      countEND = 0;
       EEPROM.writeString(JumlahMingguEEPROM, getNumber(payload, 1));
+      EEPROM.writeString(Minggu0EEPROM, "0");
       Serial.println("SETMINGGU OK");
       EEPROM.commit();
-      JadwalMinggu = EEPROM.readString(JumlahMingguEEPROM);
       lcdPrint("SETMINGGU OK", "Commit");
       buzz(500, 3);
     }
@@ -272,13 +339,17 @@ void callbackResponse(String topic, String payload){
       EEPROM.writeString(SetOnTime1MinuteEEPROM, getNumber(payload, 2));
       Serial.println("SETONTIME1 OK");
       EEPROM.commit();
-      JadwalWaktu = EEPROM.readString(SetOnTime1EEPROM);
-      JadwalWaktuMinute = EEPROM.readString(SetOnTime1MinuteEEPROM);
       lcdPrint("SETONTIME OK", "Commit");
       buzz(500, 3);
     }
     else if (command == "GETDATA"){
-      Serial.println(EEPROM.readString(SetHariEEPROM));;
+      Serial.println(EEPROM.readString(SundayEEPROM));
+      Serial.println(EEPROM.readString(MondayEEPROM));
+      Serial.println(EEPROM.readString(TuesdayEEPROM));
+      Serial.println(EEPROM.readString(WednesdayEEPROM));
+      Serial.println(EEPROM.readString(ThursdayEEPROM));
+      Serial.println(EEPROM.readString(FridayEEPROM));
+      Serial.println(EEPROM.readString(SaturdayEEPROM));
       Serial.println(EEPROM.readString(JumlahMingguEEPROM));
       Serial.println(EEPROM.readString(Minggu0EEPROM));
       Serial.println(EEPROM.readString(SetOnTime1EEPROM));
@@ -460,6 +531,7 @@ Task mqttUpdate(30000, [](){
   serializeJson(doc, telemetriData);
   sendData(publisTopic.c_str(), telemetriData.c_str()); 
 });
+
 Task serialUpdate(2000, [](){
   // = = = = = = = = = = = = = Serial Print = = = = = = = = = = = = =
   Serial.println("ID              : " + id);
@@ -489,19 +561,325 @@ void loop()
   dataUpdate.update();
   lcdUpdate.update();
   mqttUpdate.update();
-    // baca waktu dari RTC
+  // serialUpdate.update();
+  OnDay = 0;
+  SaveOn = EEPROM.readString(280).toInt();
+  if (EEPROM.readString(100) != "00:00:00") {
+    OnDay++;
+  } if (EEPROM.readString(120) != "00:00:00") {
+    OnDay++;
+  } if (EEPROM.readString(140) != "00:00:00") {
+    OnDay++;
+  } if (EEPROM.readString(160) != "00:00:00") {
+    OnDay++;
+  } if (EEPROM.readString(180) != "00:00:00") {
+    OnDay++;
+  }
   DateTime now = rtc.now();
-  // cek apakah saat ini hari yang dijadwalkan untuk menyiram
-  if (Hari[now.dayOfTheWeek()] == 1) {
-    // cek apakah sudah melewati jam penyiraman
-    if (now.hour() == JadwalWaktu.toInt().hour() && now.minute() >= TimeOn1.minute()) {
-      // aktifkan sistem penyiraman selama 30 menit
-      digitalWrite(relay1, HIGH);
-      digitalWrite(relay2, HIGH);
-      delay(30 * 60 * 1000);
+  if (now.minute() % 60 == 0) {
+    Serial.println("[580] "+String(now.minute()% 60)+" == 0");
+    // display.dim(true);
+  }
+  if (now.minute() % 60 == 1) {
+    Serial.println("[584] "+String(now.minute()% 60)+" == 1");
+    // display.dim(false);
+  }
+  if (now.second() != GlobalClock) {
+    CountDayOn = 0;
+    //    GlobalCount++;
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("[591] WiFi.status() != WL_CONNECTED");
+      Lock1 = false;
+      State1 = 0;
+    }
+    if (!client.connected()) {
+      Lock1 = false;
+      State1 = 4;
+    }
+    if (Lock1 == false) {
+      if (digitalRead(relay1) != HIGH || digitalRead(relay2) != HIGH) {
+        // digitalWrite(LED, LOW);
+        Serial.println("[601] digitalWrite(LED, LOW);");
+        Detik++;
+        if (Detik == 5) {
+          State1 = 5;
+          // ESP.restart();
+          Serial.println("[606] ESP.restart();");
+        }
+      }
+    }
+    if (EEPROM.readString(210) == "Sunday") {
+      CountDayOn++;
+    } if (EEPROM.readString(220) == "Monday") {
+      CountDayOn++;
+    } if (EEPROM.readString(230) == "Tuesday") {
+      CountDayOn++;
+    } if (EEPROM.readString(240) == "Wednesday") {
+      CountDayOn++;
+    } if (EEPROM.readString(250) == "Thursday") {
+      CountDayOn++;
+    } if (EEPROM.readString(260) == "Friday") {
+      CountDayOn++;
+    } if (EEPROM.readString(270) == "Saturday") {
+      CountDayOn++;
+    }
+    if (EEPROM.readString(200) != "END") {
+      if (EEPROM.readString(310).toInt() == CountDayOn) {
+        if(EEPROM.readString(200) != "0"){
+        int YY = EEPROM.readString(205).toInt();
+        YY++;
+        EEPROM.writeString(205, String() + YY);
+        }
+        EEPROM.writeString(320, "0000000");
+        EEPROM.writeString(310, "0");
+      }
+      if (LockTime > 0 && Lock4 == false) {
+        digitalWrite(relay1, HIGH);
+        digitalWrite(relay2, HIGH);
+        int KK = EEPROM.readString(280).toInt();
+        KK++;
+        EEPROM.writeString(280, String() + KK);
+        if (EEPROM.readString(280).toInt() == OnDay) {
+          EEPROM.writeString(280, "0   ");
+          KK = EEPROM.readString(310).toInt();
+          KK++;
+          EEPROM.writeString(310, String() + KK);
+          int LL = 320 + now.dayOfTheWeek();
+          EEPROM.writeChar(LL, '1');
+        }
+        // Send();
+        Serial.println("[650] Send();");
+        Lock4 = true;
+      }
+    }
+    if (now.hour() == 12) {
+      int LL = 320 + now.dayOfTheWeek();
+      EEPROM.writeChar(LL, '1');
+    }
+    if (EEPROM.readString(205) == EEPROM.readString(200) && EEPROM.readString(200) != 0) {
+      EEPROM.writeString(200, "END");
+      EEPROM.writeString(205, "0");
+    }
+    if (Lock4 == true) {
+      TimeMotor++;
+    }
+    if (TimeMotor >= CountMotor * 60 && Lock4 == true) {
       digitalWrite(relay1, LOW);
       digitalWrite(relay2, LOW);
+      // Send();
+      Serial.println("[669] Send();");
+      Lock4 = false;
+      LockTime = 0;
+      TimeMotor = 0;
     }
+    // display.clearDisplay();
+    // display.setTextSize(1);
+    // display.setTextColor(SSD1306_WHITE);
+    Serial.println("[677] display.clearDisplay();");
+    lcdPrint("Display", "Clear");
+    String Load = String() + daysOfTheWeek[now.dayOfTheWeek()] + ", ";
+    if (now.day() < 10) {
+      Load += String() + "0" + now.day();
+    } else {
+      Load += now.day(), DEC;
+    }
+    Load += "/";
+    if (now.month() < 10) {
+      Load += String() + "0" + now.month();
+    } else {
+      Load += now.month(), DEC;
+    }
+    Load += String() + "/" + now.year();
+    if (Load.length() >= 21) {
+      // display.setCursor(0, 0);
+      Serial.println("[694] display.setCursor(0, 0);");
+    } else if (Load.length() == 20) {
+      // display.setCursor(3, 0);
+      Serial.println("[697] display.setCursor(3, 0);");
+    } else if (Load.length() == 19) {
+      // display.setCursor(5, 0);
+      Serial.println("[700] display.setCursor(5, 0);");
+    } else if (Load.length() == 18) {
+      // display.setCursor(8, 0);
+      Serial.println("[703] display.setCursor(8, 0);");
+    }
+    Load += "\n      ";
+    if (now.hour() < 10) {
+      Load += String() + "0" + now.hour();
+    } else {
+      Load += now.hour(), DEC;
+    }
+    Load += ":";
+    if (now.minute() < 10) {
+      Load += String() + "0" + now.minute();
+    } else {
+      Load += now.minute(), DEC;
+    }
+    Load += ":";
+    if (now.second() < 10) {
+      Load += String() + "0" + now.second();
+    } else {
+      Load += now.second(), DEC;
+    }
+    //    Load += "\n\nDHT1   :\nDHT2   :\nMOTOR1 :\nMOTOR2 :\n   WIFI CONNECTED\nA";
+    if (now.second() % 10 <= 4) {
+      h1 = 123;
+      t1 = 123;
+      h2 = 456;
+      t2 = 456;
+
+      if (isnan(h1) || isnan(t1)) {
+        Serial.println(F("Failed to read from DHT sensor 1!"));
+        Load += "\n\nDHT1   :NOT CONNECT\n";
+      } else {
+        Load += String() + "\n\nDHT1  :" + t1 + "'C " + h1 + "%\n";
+      }
+      if (isnan(h2) || isnan(t2)) {
+        Serial.println(F("Failed to read from DHT sensor 2!"));
+        Load += "DHT2   :NOT CONNECT\n";
+      } else {
+        Load += String() + "DHT2  :" + t2 + "'C " + h2 + "%\n";
+      }
+      if (digitalRead(relay1) == 1) {
+        Load += String() + "RELAY1:NYALA\n";
+      } else {
+        Load += String() + "RELAY1:MATI\n";
+      }
+      if (digitalRead(relay2) == 1) {
+        Load += String() + "RELAY2:NYALA\n";
+      } else {
+        Load += String() + "RELAY2:MATI\n";
+      }
+      if (State1 == 0) {
+        Load += "  WIFI NOT CONNECTED";
+      } else if (State1 == 2) {
+        Load += "   WIFI CONNECTED";
+      } else if (State1 == 3) {
+        Load += "    CONNECTION OK";
+      } else if (State1 == 4) {
+        Load += "    NO CONNECTION";
+      } else if (State1 == 5) {
+        Load += "    RESTARTING...";
+        // display.println(Load);
+        Serial.println("[763] display.println(Load);");
+        // display.display();
+        lcdPrint("R E S T A R T", "1 1 1 1 1 1 ");
+        delay(1000);
+        // ESP.restart();
+        Serial.println("[768] ESP.restart();");
+      }
+    } else {
+      Load += "\n\nDEV :NF01";
+      Load += String() + "\nSSID:" + ssid;
+      Load += "\nSERV:broker.hivemq";
+      Load += "\nPORT:1883\n";
+      if (State1 == 0) {
+        Load += "  WIFI NOT CONNECTED";
+      } else if (State1 == 2) {
+        Load += "   WIFI CONNECTED";
+      } else if (State1 == 3) {
+        Load += "    CONNECTION OK";
+      } else if (State1 == 4) {
+        Load += "    NO CONNECTION";
+      } else if (State1 == 5) {
+        Load += "    RESTARTING...";
+        // display.println(Load);
+        // display.display();
+        lcdPrint("R E S T A R T", "2 2 2 2 22 ");
+        delay(1000);
+        // ESP.restart();
+        Serial.println("[790] ESP.restart();");
+      }
+    }
+    //    Serial.println(Load);
+    // display.println(Load);
+    // display.display();
+    lcdPrint(Load.substring(0, 16), Load.substring(16, 32));
+    client.loop();
+    GlobalClock = now.second();
   }
-  // serialUpdate.update();
+  if (digitalRead(relay1) == HIGH || digitalRead(relay2) == HIGH) {
+    MotorLock = true;
+  } else {
+    MotorLock = false;
+  }
+  if (now.hour() < 10) {
+    TimeNOW = String() + "0" + now.hour();
+  } else {
+    TimeNOW = now.hour(), DEC;
+  }
+  TimeNOW += ":";
+  if (now.minute() < 10) {
+    TimeNOW += String() + "0" + now.minute();
+  } else {
+    TimeNOW += now.minute(), DEC;
+  }
+  TimeNOW += ":";
+  if (now.second() < 10) {
+    TimeNOW += String() + "0" + now.second();
+  } else {
+    TimeNOW += now.second(), DEC;
+  }
+  if (EEPROM.readString(200) != "END") {
+    TotalMinggu = EEPROM.readString(200).toInt();
+    DateNOW = daysOfTheWeek[now.dayOfTheWeek()];
+    if (DateNOW == EEPROM.readString(210) || DateNOW == EEPROM.readString(220) || DateNOW == EEPROM.readString(230) || DateNOW == EEPROM.readString(240) || DateNOW == EEPROM.readString(250) || DateNOW == EEPROM.readString(260) || DateNOW == EEPROM.readString(270)) {
+      LockDate = true;
+    } else {
+      LockDate = false;
+    }
+    if (LockDate == true && CountMinggu <= TotalMinggu && EEPROM.readString(200).toInt() > 0) {
+      if (TimeNOW == EEPROM.readString(100) && EEPROM.readString(100) != "00:00:00") {
+        LockTime = 1;
+      } else if (TimeNOW == EEPROM.readString(120) && EEPROM.readString(120) != "00:00:00") {
+        LockTime = 2;
+      } else if (TimeNOW == EEPROM.readString(140) && EEPROM.readString(140) != "00:00:00") {
+        LockTime = 3;
+      } else if (TimeNOW == EEPROM.readString(160) && EEPROM.readString(160) != "00:00:00") {
+        LockTime = 4;
+      } else if (TimeNOW == EEPROM.readString(180) && EEPROM.readString(180) != "00:00:00") {
+        LockTime = 5;
+      }
+      if (LockTime == 1) {
+        CountMotor = EEPROM.readString(110).toInt();
+      } else if (LockTime == 2) {
+        CountMotor = EEPROM.readString(130).toInt();
+      } else if (LockTime == 3) {
+        CountMotor = EEPROM.readString(150).toInt();
+      } else if (LockTime == 4) {
+        CountMotor = EEPROM.readString(170).toInt();
+      } else if (LockTime == 5) {
+        CountMotor = EEPROM.readString(190).toInt();
+      }
+    }
+    countEND = 0;
+  }
+  if (EEPROM.readString(200) == "END") {
+    EEPROM.writeString(200, "END");
+    EEPROM.writeString(205, "0");
+    EEPROM.writeString(280, "0");
+    EEPROM.writeString(310, "0");
+    EEPROM.writeString(320, "0000000");
+  }
+  //  Serial.println(String() + "DATA SET ON 1: " + EEPROM.readString(100) + " MINUTE : " + EEPROM.readString(110));
+  //  Serial.println(String() + "DATA SET ON 2: " + EEPROM.readString(120) + " MINUTE : " + EEPROM.readString(130));
+  //  Serial.println(String() + "DATA SET ON 3: " + EEPROM.readString(140) + " MINUTE : " + EEPROM.readString(150));
+  //  Serial.println(String() + "DATA SET ON 4: " + EEPROM.readString(160) + " MINUTE : " + EEPROM.readString(170));
+  //  Serial.println(String() + "DATA SET ON 5: " + EEPROM.readString(180) + " MINUTE : " + EEPROM.readString(190));
+  //  Serial.println(String() + "DATA MINGGU : " + EEPROM.readString(200));
+  //  Serial.println(String() + "DATA CURRENT MINGGU: " + EEPROM.readString(205));
+  //  Serial.println(String() + "DATA MINGGU: " + EEPROM.readString(210));
+  //  Serial.println(String() + "DATA SENIN: " + EEPROM.readString(220));
+  //  Serial.println(String() + "DATA SELASA: " + EEPROM.readString(230));
+  //  Serial.println(String() + "DATA RABU: " + EEPROM.readString(240));
+  //  Serial.println(String() + "DATA KAMIS: " + EEPROM.readString(250));
+  //  Serial.println(String() + "DATA JUMAT: " + EEPROM.readString(260));
+  //  Serial.println(String() + "DATA SABTU: " + EEPROM.readString(270));
+  //  Serial.println(String() + "ONDAY: " + OnDay);
+  //  Serial.println(String() + "SAVEON: " + EEPROM.readString(280));
+  //  Serial.println(String() + "COUNT DAY ON: " + EEPROM.readString(310));
+  //  Serial.println(String() + "STRING DAY ON: " + EEPROM.readString(320));
+  //  Serial.println("\n\n");
+  EEPROM.commit();
+  CountDayOn = 0;
 }
